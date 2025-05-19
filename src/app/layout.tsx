dbf54d1e1e383f988b5f485db77c31594934ad86
Mouaad Idoufkir <mouaadidoufkir2@gmail.com>
2025-05-19 18:58:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: Hydration failed because the server rendered HTML didn't match the client. As a result this tree will be regenerated on the client. This can happen if a SSR-ed Client Component used:. Error source: - A server/client branch `if (typeof window !== 'undefined')`.
    > - Variable input such as `Date.now()` or `Math.random()` which changes each time it's called.
    > - Date formatting in a user's locale which doesn't match the server.
    > - External changing data without sending a snapshot of it along with the HTML.
    > - Invalid HTML tag nesting.
    >
    > It can also happen if the client has a browser extension installed which messes with the HTML before React loaded.
    >
    > See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    >   ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <RootLayout>
    >                       <html lang="en" suppressHydrationWarning={true}>
    >                         <body
    >                           className="geist_e531dabc-module__QGiZLq__variable font-sans antialiased"
    > -                         style={{}}
    >                         >
    >                     ...
    >         ...
    >
    >
    > src/app/layout.tsx (24:7) @ RootLayout
    >
    >   22 |   return (
    >   23 |     <html lang="en" suppressHydrationWarning>
    > > 24 |       <body className={`${geistSans.variable} font-sans antialiased`}>
    >      |       ^
    >   25 |         <ThemeProvider>
    >   26 |           {children}
    >   27 |           <Toaster />
    >
    > Call Stack
    > 17
    >
    > Show 15 ignore-listed frame(s)
    > body
    > <anonymous> (0:0)
    > RootLayout
    > src/app/layout.tsx (24:7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} font-sans antialiased`}>
+      <body className={`${geistSans.variable} font-sans antialiased`} suppressHydrationWarning>
         <ThemeProvider>
           {children}
           <Toaster />
