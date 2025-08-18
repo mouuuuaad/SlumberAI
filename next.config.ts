f95d8e961ee59ec898bec1100041691329b8b215
Mouaad idoufkir - معاذ ادافقير <152400161+mouuuuaad@users.noreply.github.com>
2025-08-18 16:29:19 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n.ts'); // Provide path to i18n.ts
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
   typescript: {
     ignoreBuildErrors: true,
   },
